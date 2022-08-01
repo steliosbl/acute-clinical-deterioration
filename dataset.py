2aee1f6cf68c09fff36e05f19ee5e7f801d856ff
Stelios Boulitsakis Logothetis <stelios@logothet.is>
2022-08-01 20:18:11 +0100

    Added further cleaning

--- dataset.py
+++ dataset.py
@@ -30,6 +30,20 @@ class SCIData(pd.DataFrame):
             .derive_main_icd3code(force=force)
         )
 
+    def clean_all(self):
+        return (
+            self.clean_blood_pressure()
+            .clean_consciousness()
+            .clean_device_air()
+            .clean_heart_rate()
+            .clean_O2_flow()
+            .clean_O2_saturation()
+            .clean_respiration_rate()
+            .clean_temperature()
+            .clean_ae_text()
+            .clean_ae_patient_group()
+        )
+
     def derive_covid(
         self,
         covid_codes=["U07.1", "J12.8", "B97.2"],
@@ -69,6 +83,7 @@ class SCIData(pd.DataFrame):
         self,
         bins=[0, 1, 2, 7, 14, 30, 60],
         labels=["24 Hrs", "48 Hrs", "1 Week", "2 Weeks", "1 Month", "2 Months"],
+        readmission_thresh=30,
         force=False,
     ):
         """Determines the timespan since the patient's last admission. Bins readmissions into bands.
@@ -88,18 +103,21 @@ class SCIData(pd.DataFrame):
         bins = [pd.Timedelta(days=_) for _ in bins]
 
         r = self.copy()
-        r["ReadmissionTimespan"] = (
-            r.sort_values(["PatientNumber", "AdmissionDateTime"])
-            .groupby("PatientNumber")
-            .AdmissionDateTime.diff()
-            .dropna()
+        grouped = r.sort_values(["PatientNumber", "AdmissionDateTime"]).groupby(
+            "PatientNumber"
         )
+        r["ReadmissionTimespan"] = grouped.AdmissionDateTime.diff().dropna()
         r["ReadmissionBand"] = (
             pd.cut(r.ReadmissionTimespan, bins, labels=labels)
             .astype(str)
             .replace("nan", np.nan)
         )
-        r["Readmission"] = r.ReadmissionTimespan < pd.Timedelta(days=14)
+        r["Readmission"] = r.ReadmissionTimespan < pd.Timedelta(days=readmission_thresh)
+
+        timespan_reverse = grouped.AdmissionDateTime.diff(-1).dropna()
+        r["Readmitted"] = (
+            timespan_reverse > pd.Timedelta(days=-readmission_thresh)
+        ).fillna(False)
 
         return SCIData(data=r)
 
@@ -142,6 +160,7 @@ class SCIData(pd.DataFrame):
         r.CriticalReadmission = r.CriticalReadmission.eq("both") | (
             r.Readmission & r.CriticalCare
         )
+        r.CriticalReadmitted = r.Readmitted & r.CriticalCare
 
         return SCIData(data=r.drop("value", axis=1))
 
@@ -188,6 +207,401 @@ class SCIData(pd.DataFrame):
         r.loc[r.MainICD10.str[:3].isin(vague), "MainICD10"] = np.nan
 
         return SCIData(data=r)
+
+    def clean_O2_saturation(self, outlier_threshold_std=3):
+        sat, score = "c_O2_saturation", "c_NEWS_O2_sat_score"
+        r = self.copy()
+
+        # Assume negatives were erroneous and make them positive
+        r.loc[r[sat] < 0, sat] *= -1
+
+        # Assume zeros are dead
+        r.loc[r[sat] == 0, sat] = np.nan
+
+        # Assume single-digits were erroneous and shift to the left (e.g. 9.0 -> 90)
+        r.loc[r[sat] <= 10, sat] *= 10
+
+        # Set threshold for outliers to be under 3 std of the mean OR anything >100 (as it is impossible)
+        threshold = r[sat].mean() - 3 * r[sat].std()
+        outlier = (r[sat] > 100) | (r[sat] <= threshold)
+
+        # Set criterion for SpO2_2 scale to be presence of COPD in the coded diagnoses
+        copd = (
+            r[SCICols.diagnoses]
+            .isin(["J44.0", "J44.1", "J44.8", "J44.9", "J44.X"])
+            .any(axis=1)
+        )
+
+        # Set outliers without copd and O2 Sat score < 3 to the midpoint of the corresponding score range
+        mask1 = outlier & ~copd
+        r.loc[mask1, sat] = np.nan
+
+        # Set outliers with copd and O2 Sat score = 0 to 92
+        mask2 = outlier & copd & (r[score] == 0)
+        r.loc[mask2, sat] = 92
+
+        # Delete the remaining outliers
+        r.loc[outlier & copd & ~mask2, sat] = np.nan
+
+        # Missing: Populate missing O2 saturation from score if available
+        missing_sat_mask = r[sat].isna() & r[score].notna()
+        missing_sat_scale1, missing_sat_scale2 = (
+            ~copd & missing_sat_mask,
+            copd & (r[score] == 0) & missing_sat_mask,
+        )
+
+        r.loc[missing_sat_scale1, sat] = r.loc[missing_sat_scale1, score].apply(
+            NEWS.SpO2_1_Scale_reverse
+        )
+        r.loc[missing_sat_scale2, sat] = 92
+
+        # Missing: Populate missing O2 score from saturation if available
+        missing_score_mask = r[sat].notna() & r[score].isna()
+        missing_score_scale1, missing_score_scale2 = (
+            ~copd & missing_score_mask,
+            copd & missing_score_mask,
+        )
+
+        r.loc[missing_score_scale1, score] = r.loc[missing_score_scale1, sat].apply(
+            NEWS.SpO2_1_Scale
+        )
+        r.loc[missing_score_scale2, score] = r.loc[missing_score_scale2].apply(
+            lambda row: NEWS.SpO2_2_Scale(row[sat], row["c_Assisted_breathing"]), axis=1
+        )
+
+        return SCIData(r)
+
+    def clean_blood_pressure(self, outlier_threshold_std=3):
+        sys, dia, score = "c_BP_Systolic", "c_BP_Diastolic", "c_NEWS_BP_score"
+        r = self.copy()
+
+        for col in [sys, dia]:
+            # Assume negatives were erroneous and make them positive
+            r.loc[r[col] < 0, col] *= -1
+
+        # Delete values outside cutoff range
+        outlier_dia = (r[dia] <= 5) | (r[dia] >= 200)
+        r.loc[outlier_dia, dia] = np.nan
+
+        outlier_sys = (r[sys] <= 20) | (r[sys] >= 300) | (r[sys] <= r[dia] + 5)
+        r.loc[outlier_sys, sys] = np.nan
+
+        # Fill missing values using score and vice versa
+        missing_score_mask, missing_sys_mask = (
+            r[score].isna() & r[sys].notna(),
+            r[score].notna() & r[sys].isna(),
+        )
+
+        r.loc[missing_sys_mask, sys] = r.loc[missing_sys_mask, score].apply(
+            NEWS.systolic_scale_reverse
+        )
+        r.loc[missing_score_mask, score] = r.loc[missing_score_mask, sys].apply(
+            NEWS.systolic_scale
+        )
+
+        return r
+
+    def clean_respiration_rate(self):
+        rate, score = "c_Respiration_rate", "c_NEWS_resp_rate_score"
+        r = self.copy()
+
+        # Assume negatives are erroneous and make them positive
+        r.loc[r[rate] < 0, rate] *= -1
+
+        # Assume triple digits are erroneous and shift them to the left
+        r.loc[r[rate] >= 100, rate] //= 10
+
+        outliers = (r[rate] >= 80) | (r[rate] <= 5)
+        r.loc[outliers, rate] = np.nan
+
+        missing_score_mask, missing_rate_mask = (
+            r[score].isna() & r[rate].notna(),
+            r[rate].isna() & r[score].notna(),
+        )
+
+        r.loc[missing_score_mask, score] = r.loc[missing_score_mask, rate].apply(
+            NEWS.respiration_scale
+        )
+        r.loc[missing_rate_mask, rate] = r.loc[missing_rate_mask, score].apply(
+            NEWS.respiration_scale_reverse
+        )
+
+        return SCIData(r)
+
+    def clean_device_air(self):
+        score, value = "c_NEWS_device_air_score", "c_Assisted_breathing"
+        r = self.copy()
+
+        # Some erroneous 1.0 values (impossible per the scale)
+        r.loc[r[score] == 1.0, score] = np.nan
+
+        missing_score_mask, missing_value_mask = (
+            r[value].notna() & r[score].isna(),
+            r[score].notna() & r[value].isna(),
+        )
+
+        r.loc[missing_score_mask, score] = r.loc[missing_score_mask, value].map(
+            {True: 2.0, False: 0.0}
+        )
+
+        # Assume the score is more correct
+        r[value] = r[score].map({2.0: True, 0.0: False})
+
+        return SCIData(r)
+
+    def clean_temperature(self):
+        value, score = "c_Temperature", "c_NEWS_temperature_score"
+        r = self.copy()
+
+        r.loc[r[value] < 0, value] *= 1
+
+        outliers = (r[value] > 45) | (r[value] < 25)
+        r.loc[outliers, value] = np.nan
+
+        missing_value_mask, missing_score_mask = (
+            r[value].isna() & r[score].notna(),
+            r[score].isna() & r[value].notna(),
+        )
+
+        r.loc[missing_value_mask, value] = r.loc[missing_value_mask, score].apply(
+            NEWS.temperature_scale_reverse
+        )
+        r.loc[missing_score_mask, score] = r.loc[missing_score_mask, value].apply(
+            NEWS.temperature_scale
+        )
+
+        return SCIData(r)
+
+    def clean_heart_rate(self):
+        score, value = "c_NEWS_heart_rate_score", "c_Heart_rate"
+        r = self.copy()
+
+        r.loc[r[value] < 0, value] *= 1
+
+        outliers = (r[value] < 5) | (r[value] > 300)
+        r.loc[outliers, value] = np.nan
+
+        missing_value_mask, missing_score_mask = (
+            r[value].isna() & r[score].notna(),
+            r[score].isna() & r[value].notna(),
+        )
+
+        r.loc[missing_value_mask, value] = r.loc[missing_value_mask, score].apply(
+            NEWS.heartrate_scale_reverse
+        )
+        r.loc[missing_score_mask, score] = r.loc[missing_score_mask, value].apply(
+            NEWS.heartrate_scale
+        )
+
+        return SCIData(r)
+
+    def clean_consciousness(self):
+        score, value = "c_NEWS_level_of_con_score", "c_Alert"
+        r = self.copy()
+
+        # Some erroneous 1.0 values (impossible per the scale)
+        r.loc[r[score] == 1.0, score] = np.nan
+
+        missing_score_mask, missing_value_mask = (
+            r[value].notna() & r[score].isna(),
+            r[score].notna() & r[value].isna(),
+        )
+
+        r.loc[missing_score_mask, score] = r.loc[missing_score_mask, value].map(
+            {False: 3.0, True: 0.0}
+        )
+
+        # Assume the score is more correct
+        r[value] = r[score].map({3.0: False, 0.0: True})
+
+        return SCIData(r)
+
+    def clean_O2_flow(self):
+        col = "c_Oxygen_flow_rate"
+        r = self.copy()
+
+        r.loc[r[col] > 10, col] /= 10
+
+        return r
+
+    def clean_ae_text(self):
+        complaint, diag = "AandEPresentingComplaint", "AandEMainDiagnosis"
+        r = self.copy()
+
+        vague = [
+            "referral to service (procedure)",
+            "generally unwell (finding)",
+            "unwell adult",
+            "unknown",
+            "other",
+            "general deterioration",
+            "generally unwell",
+            "gen unwell",
+        ]
+        for col in [complaint, diag]:
+            r[col] = r[col].str.lower().str.strip(" .?+")
+            r.loc[r[col].isin(vague), col] = np.nan
+
+        return SCIData(r)
+
+    def clean_ae_patient_group(self):
+        r = self.copy()
+        col = "AandEPatientGroupDescription"
+
+        r[col] = r[col].str.lower()
+        r[col] = r[col].replace(
+            {
+                "other than above": "other",
+                "falls": "accident",
+                "road traffic accident": "accident",
+                "sports injury": "accident",
+                "knife injuries inflicted": "assault",
+            }
+        )
+
+        return SCIData(r)
+
+
+class NEWS:
+    @staticmethod
+    def SpO2_1_Scale_reverse(score):
+        if score == 2:
+            return 93
+        elif score == 1:
+            return 95
+        elif score == 0:
+            return 98
+        elif score == 3:
+            return np.nan
+
+    @staticmethod
+    def SpO2_1_Scale(sat):
+        if sat >= 96:
+            return 0
+        elif sat >= 94:
+            return 1
+        elif sat >= 92:
+            return 2
+        else:
+            return 3
+
+    @staticmethod
+    def SpO2_2_Scale(sat, assisted):
+        if assisted:
+            if sat >= 97:
+                return 3
+            elif sat >= 95:
+                return 2
+            elif sat >= 93:
+                return 1
+            elif sat >= 88:
+                return 0
+        if sat >= 93:
+            return 0
+        elif sat >= 86:
+            return 1
+        elif sat >= 84:
+            return 2
+        else:
+            return 3
+
+    @staticmethod
+    def systolic_scale(sys):
+        if sys >= 220:
+            return 3
+        elif (sys <= 219) and (sys >= 111):
+            return 0
+        elif (sys <= 110) and (sys >= 101):
+            return 1
+        elif (sys <= 100) and (sys >= 91):
+            return 2
+        elif sys <= 90:
+            return 3
+
+    @staticmethod
+    def systolic_scale_reverse(score):
+        if score == 2:
+            return 95
+        elif score == 1:
+            return 105
+        elif score == 0:
+            return 165
+        else:
+            return np.nan
+
+    @staticmethod
+    def respiration_scale_reverse(score):
+        if score == 1:
+            return 10
+        elif score == 2:
+            return 23
+        elif score == 0:
+            return 16
+        else:
+            return np.nan
+
+    @staticmethod
+    def respiration_scale(rate):
+        if rate >= 25:
+            return 3
+        elif rate >= 21:
+            return 2
+        elif rate >= 12:
+            return 0
+        elif rate >= 9:
+            return 1
+        else:
+            return 3
+
+    @staticmethod
+    def temperature_scale(temperature):
+        if temperature >= 39.1:
+            return 2
+        elif temperature >= 38.1:
+            return 1
+        elif temperature >= 36.1:
+            return 0
+        elif temperature >= 35.1:
+            return 1
+        else:
+            return 3
+
+    @staticmethod
+    def temperature_scale_reverse(score):
+        if score == 2:
+            return 39.1
+        elif score == 0:
+            return 37.0
+        elif score == 1:
+            return 37.0
+        elif score == 3:
+            return 35.0
+
+    @staticmethod
+    def heartrate_scale(rate):
+        if rate >= 131:
+            return 3
+        elif rate >= 111:
+            return 2
+        elif rate >= 91:
+            return 1
+        elif rate >= 51:
+            return 0
+        elif rate >= 41:
+            return 1
+        else:
+            return 3
+
+    @staticmethod
+    def heartrate_scale_reverse(score):
+        if score == 1:
+            return 70
+        elif score == 0:
+            return 70
+        elif score == 2:
+            return 120
+        else:
+            return np.nan
 
 
 class SCICols:
@@ -333,7 +747,7 @@ class SCICols:
         "Creatinine",
     ]
 
-    vgb = [
+    vbg = [
         "PatientTemperatureVenous",
         "pCO2(POC)Venous",
         "pCO2(Tempcorrected)(POC)Venous",
